c5ba92d9288a8b827c0a74f508b474576ae68a34
Mhmmdrzk <118596896+Mhmmdrzk@users.noreply.github.com>
2022-12-05 10:11:34 +0700

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+ 	
+ 	cout<<"NAMA : MUHAMMAD RIZKI ABDUSSALAM"<<endl;
+ 	cout<<"NO HP : 085723996042"<<endl;
+ 	cout<<"JENIS KELAMIN : LAKI LAKI"<<endl;
+ 	
+ 	return 0;
+ 	
+ }
